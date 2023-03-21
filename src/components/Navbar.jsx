0296171c97cc41aedff0172957e5a257a61cc410
JC <j.c.ashley4363@gmail.com>
2023-03-20 20:23:24 -0500

    header fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import logo2 from '../assets/logo2.jpg'
 function Navbar() {
     return (
         <header>
-            <div className='flex items-center flex-wrap flex-grow justify-evenly w-full h-fit bg-slate-100 text-shadow px-[10%]'>
+            <div className='flex items-center flex-wrap flex-grow justify-evenly w-full h-fit bg-white text-shadow px-[10%]'>
                 <div className='flex items-center'>
                     <div className='relative'>
                         <Link to='/'>
@@ -13,7 +13,7 @@ function Navbar() {
                         </Link>
                     </div>
                     <div className='text-center'>
-                        <h1 className='font-lora font-extrabold text-2xl text-center shadow-md shadow-amber-300'>Top Flight Lawn Service</h1>
+                        <h1 className='font-lora font-extrabold text-2xl text-center shadow-md md:text-4xl lg:text-5xl shadow-gray-700'>Top Flight Lawn Service</h1>
                     </div>
                 </div>
 
